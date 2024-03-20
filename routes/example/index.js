6b7dec05f89d323db009897b71eb649f8998a139
Daniel <hnnk@qq.com>
2024-03-20 15:06:30 +0800

    test

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,7 +1,7 @@
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    let result = await fastify.oracle11g.execute(`SELECT * FROM CRM_CustomerInfo`);
-    console.log(result);
-    return result.rows;
+    // let result = await fastify.oracle11g.execute(`SELECT * FROM CRM_CustomerInfo`);
+    // console.log(result);
+    return "this is an example";
   })
 }
