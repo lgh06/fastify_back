e341a25df25f1ced41cd1529367d3117f758c2a2
Daniel <hnnk@qq.com>
2024-03-21 10:16:04 +0800

    new config

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,21 @@
 const ORACLE_DB_CONFIG = {
   binaryDir: 'E:\exe\instantclient_12_2',
-  user: 'ecology', 
-  password: '',
-  connectString: ''//Database address: {IP: PORT/Database name}
+  user: 'z', 
+  password: 'x',
+  connectString: 'y'//Database address: {IP: PORT/Database name}
 }
 
-const MSSQL_DB_CONN_STR = "Server=xx,1433;Database=yy;User Id=zz;Password=aa;Encrypt=false";
+const MSSQL_DB_CONN_STR = "Server=x,1433;Database=y;User Id=z;Password=z;Encrypt=false";
 
-export { ORACLE_DB_CONFIG, MSSQL_DB_CONN_STR }
+
+const MSSQL_DB_CONFIG = {
+  user: 'x', 
+  password: 'y',
+  server: 'z', 
+  database: 'z',
+  port: 1433,
+  connectString: MSSQL_DB_CONN_STR
+}
+
+
+export { ORACLE_DB_CONFIG, MSSQL_DB_CONN_STR, MSSQL_DB_CONFIG }
