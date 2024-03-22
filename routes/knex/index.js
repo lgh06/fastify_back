475424c50c0cf62cd3f075e2dc6d609257c9aa83
Daniel <hnnk@qq.com>
2024-03-22 15:50:21 +0800

    sql

--- routes/knex/index.js
+++ routes/knex/index.js
@@ -2,12 +2,12 @@ import {knexMssql, knexOracle} from "../../helpers/index.mjs";
 
 export default async function (fastify, opts) {
   fastify.get('/1', async function (request, reply) {
-    let result = await knexOracle.select(knexOracle.raw("id,name")).fromRaw("CRM_CustomerInfo").limit(20);
+    let result = await knexOracle.select(knexOracle.raw("id,name")).fromRaw("CRM_CustomerInfo").orderByRaw("id asc").limit(20);
     return result;
   })
 
   fastify.get('/2', async function (request, reply) {
-    let result = await knexMssql.select(knexOracle.raw("*")).from(`UFDATA_999_2018.dbo.CusDeliverAdd`).limit(10);
+    let result = await knexMssql.select("*").from(`UFDATA_999_2018.dbo.CusDeliverAdd`).orderBy("id","desc").limit(10);
     return result;
   })
 }
